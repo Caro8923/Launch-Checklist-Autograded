425838461d81af0aa7cef0acff7e9f3685240ea1
Carolyn Smith <carolynsmith@slu.edu>
2024-08-14 13:54:07 -0500

    Organized scriptHelper and script; passes 5/10 tests

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 // Write your JavaScript code here!
 
+const { addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault()
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    })
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse = planetsReturned;
+    let listedPlanetsResponse = planetsReturned;
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        let planet;
-        return planet = pickPlanet(listedPlanetsResponse);
+        let planet = pickPlanet(listedPlanetsResponse);
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let imageUrl = planet.image;
+        let moons = planet.moons;
+        addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
     })
-    
+
  });
+
+ /* Questions : how/where do i access list?
+                how do i validate testInput bc it is automatically a string?
+                Why is my test input not working?
+                floating else statement if shuttle ready to launch?
--- scriptHelper.js
+++ scriptHelper.js
@@ -31,67 +31,53 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
    
  
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    window.addEventListener("load", function(){
-        let form = document.querySelector("form");
-        form.addEventListener("submit", function(event) {
+ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
+    
+    //use validate input function to ensure proper data entry (forms filled out, pilot and copilot are strings, fuel and cargo are numbers)
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty") {
+        alert("Please enter data for each subject.");
+    }
+    if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
+        alert("Please ensure pilot and copilot are non-numerical entries.")
+    }
+    if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoMass) === "Not a Number") {
+        alert("Please ensure Fuel Level and Cargo Mass are numerical entries.")
+    }
+
+
         
-            //validate that user enter correct data type
-        let pilotName = document.querySelector("input[name=pilotName]");
-        let copilotName = document.querySelector("input[name=copilotName]");
-        let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoMass = document.querySelector("input[name=cargoMass]");
-        if (typeof(pilotName.value) !== string) {
-            alert("Pilot Name invalid");
-            event.preventDefault();
-        }
-        if (typeof(copilotName.value) !== string) {
-            alert("Copilot Name invalid");
-            event.preventDefault();
-            }
-        if (typeof(fuelLevel.value) !== number) {
-            alert("Fuel Level invalid");
-            event.preventDefault();
-        }
-        if (typeof(cargoMass.value) !== number) {
-            alert("Cargo Mass invalid");
-            event.preventDefault();
-        }
-        
-        //update requirements
+     //update requirements
 
-        document.getElementbyId("pilotStatus").innerHTML = `Pilot ${pilot} Ready`;
-        document.getElementbyId("copilotStatus").innerHTML = `Co-pilot ${copilot} Ready`
+    document.getElementbyId("pilotStatus").innerHTML = `Pilot ${pilot} Ready`;
+    document.getElementbyId("copilotStatus").innerHTML = `Co-pilot ${copilot} Ready`
 
-        if (fuelLevel < 10000) {
-            document.getElementbyId("faultyitems").style.visibility = "visible";
-            document.getElementbyId("fuelStatus").innerHTML = "Not enough fuel for the journey. Please refuel.";
-            document.getElementbyId("launchStatus").innerHTML = "Shuttle not ready for launch.";
-            document.getElementbyId("launchStatus").style.color = "red";
+    if (Number(fuelLevel) < 10000) {
+         document.getElementbyId("faultyItems").style.visibility = "visible";
+         document.getElementbyId("fuelStatus").innerHTML = "Not enough fuel for the journey. Please refuel.";
+         document.getElementbyId("launchStatus").innerHTML = "Shuttle not ready for launch.";
+         document.getElementbyId("launchStatus").style.color = "red";
         }
 
-        if (cargoMass > 10000) {
-            document.getElementbyId("faultyitems").style.visiblity = "visible";
-            document.getElementbyId("faultyitems").innerHTML = "There is too much mass for shuttle takeoff.";
-            document.getElementbyId("launchStatus").innerHTML = "Shuttle not ready for launch.";
-            document.getElementbyId("launchStatus").style.color = "red";
+    if (Number(cargoMass) > 10000) {
+        document.getElementbyId("faultyItems").style.visiblity = "visible";
+        document.getElementbyId("cargoStatus").innerHTML = "There is too much mass for shuttle takeoff.";
+        document.getElementbyId("launchStatus").innerHTML = "Shuttle not ready for launch.";
+        document.getElementbyId("launchStatus").style.color = "red";
         }
 
         // shuttle ready to launch
 
-        else {
-            document.getElementbyId("launchStatus").innerHTML = "Shuttle is ready for launch.";
-            document.getElementbyId("launchStatus").style.color = "green";
+    if (Number(fuelLevel) < 10000 && (Number(cargoMass) < 10000)) {
+        document.getElementbyId("faultyItems").style.visibility = "hidden";
+        document.getElementbyId("launchStatus").innerHTML = "Shuttle is ready for launch.";
+        document.getElementbyId("launchStatus").style.color = "green";
         }
     
-        });
-        });
     }
  
  async function myFetch() {
      let planetsReturned;
-     fetch("https://handlers.education.launchcode.org/static/planets.json")
-     planetsReturned = await fetch().then( function(response) {
+     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
         const jsonPromise = response.json();
         return jsonPromise;
         });
